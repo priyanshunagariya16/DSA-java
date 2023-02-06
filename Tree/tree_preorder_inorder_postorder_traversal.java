da0ea30b5654a1745b144221315c318af052d80f
priyanshunagariya16 <priyanshunagaria16@gmail.com>
2023-02-06 13:33:28 +0530

    commit

---
+++ Tree/tree_preorder_inorder_postorder_traversal.java
@@ -0,0 +1,64 @@
+package Tree;
+
+public class tree_preorder_inorder_postorder_traversal {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildtree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newnode = new Node(nodes[idx]);
+            newnode.left = buildtree(nodes);
+            newnode.right = buildtree(nodes);
+            return newnode;
+        }
+        //preorder traversal
+        public static void preorder(Node root){
+            if(root==null) {
+                return;
+            }
+            System.out.print(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        //inorder traversal
+        public static void inorder(Node root){
+            if(root==null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
+
+        //postorder traversal
+        public static void postorder(Node root){
+            if(root==null){
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
+
+    }
+
+    public static void main(String[] args){
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildtree(nodes);
+        tree.preorder(root);
+        tree.inorder(root);tree.postorder(root);}
+}
