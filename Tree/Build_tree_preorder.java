8e91988096835e1bf626ad792bbbf1f84f05f295
priyanshunagariya16 <priyanshunagaria16@gmail.com>
2023-02-06 13:01:49 +0530

    commit

---
+++ Tree/Build_tree_preorder.java
@@ -0,0 +1,33 @@
+package Tree;
+
+public class Build_tree_preorder {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildtree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            Node newnode = new Node(nodes[idx]);
+            newnode.left = buildtree(nodes);
+            newnode.right  = buildtree(nodes);
+            return newnode;
+        }
+    }
+    public static void main(String[] args){
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildtree(nodes);
+        System.out.println(root.data);
+    }
+}
