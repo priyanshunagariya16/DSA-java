038ef35f7f1985965fb7d606bc3ff9213c0254c9
priyanshunagariya16 <priyanshunagaria16@gmail.com>
2023-02-13 17:39:15 +0530

    commit

---
+++ Tree/Lowest_common_ancestor_1.java
@@ -0,0 +1,42 @@
+package Tree;
+
+public class Lowest_common_ancestor_1 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    public static Node lca(Node root, int n1, int n2){
+        if(root==null || root.data==n1 || root.data==n2){
+            return root;
+        }
+        Node leftlca = lca(root.left, n1,n2);
+        Node rightlca = lca(root.right, n1, n2);
+
+        if(rightlca==null){
+            return leftlca;
+        }
+        if(leftlca==null){
+            return rightlca;
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        int n1=4;
+        int n2=6;
+        System.out.println(lca(root,n1,n2).data);
+    }
+}
