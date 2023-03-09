b75e1ca4130444f4ddd0e9a06d24e01bab6524dd
priyanshunagariya16 <priyanshunagaria16@gmail.com>
2023-03-09 19:40:39 +0530

    commit

--- Array/print_subarrays.java
+++ Array/print_subarrays.java
@@ -2,15 +2,19 @@ package Array;
 
 public class print_subarrays {
     public static void subarrays(int arr[]){
+        int totalSubarrays=0;
         for(int i=0; i<arr.length; i++){
             for(int j=i; j<arr.length; j++){
                 for(int k=i; k<=j; k++){
                     System.out.print(arr[k]+" ");
                 }
+                totalSubarrays++;
                 System.out.println();
             }
             System.out.println();
+
         }
+        System.out.println("Total number of subarrays "+totalSubarrays);
     }
     public static void main(String[] args) {
         int arr[] = {2,4,5,6,7,9};
