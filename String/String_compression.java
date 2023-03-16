8acf6f2fe5be326ed78ca12dc457d661a3dd354e
priyanshunagariya16 <priyanshunagaria16@gmail.com>
2023-03-16 11:44:22 +0530

    commit

---
+++ String/String_compression.java
@@ -0,0 +1,24 @@
+package String;
+
+public class String_compression {
+    public static String compress(String str){
+        String newstr = "";
+        for(int i=0; i<str.length(); i++){
+            Integer count = 1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+
+            }
+            newstr+=str.charAt(i);
+            if(count>1){
+                newstr+=count.toString();
+            }
+        }
+        return newstr;
+    }
+    public static void main(String[] args) {
+        String str = "aaabbcccddd";
+        System.out.println(compress(str));
+    }
+}
