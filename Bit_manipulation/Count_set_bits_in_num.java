56b62b7bfaeedd05f2ed31d3f2603e5a0cbdd25c
priyanshunagariya16 <priyanshunagaria16@gmail.com>
2023-03-30 18:16:39 +0530

    commit

---
+++ Bit_manipulation/Count_set_bits_in_num.java
@@ -0,0 +1,17 @@
+package Bit_manipulation;
+
+public class Count_set_bits_in_num {
+    public static int CountSet(int n){
+        int count = 0;
+        while(n>0){
+            if((n&1) !=0){
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+    public static void main(String args[]){
+        System.out.println(CountSet(10));
+    }
+}
