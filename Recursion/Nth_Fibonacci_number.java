be2adfa2ac67df8a22e85c190f1874dafc0e4f6a
priyanshunagariya16 <priyanshunagaria16@gmail.com>
2023-04-02 19:42:08 +0530

    commit

---
+++ Recursion/Nth_Fibonacci_number.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Nth_Fibonacci_number {
+    public static int fib(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        return fnm1+fnm2;
+    }
+    public static void main(String args[]){
+        int n = 5;
+        System.out.println(fib(n));
+    }
+}
